e0a73942285e056a48fd2d7d1704f88def7d7836
nyksy <71010049+nyksy@users.noreply.github.com>
2021-08-26 17:42:37 +0300

    viikonpäivä tapahtumalle

--- src/components/Event.js
+++ src/components/Event.js
@@ -5,6 +5,13 @@ import 'moment/locale/fi'
 import './Event.scss'
 
 export default function Event(props) {
+
+  const sutkautuksia = [
+    'Onpa täällä tyhjää :-)',
+    'Mitäpä tähän nyt selittämään :-)'
+  ]
+
+
   const location = () => {
     if (props.location && props.location.split(',').length > 1) {
       return <h5>{props.location.split(',')[0]}</h5>
@@ -26,12 +33,30 @@ export default function Event(props) {
     }
   }
 
+  const day = () => {
+    if (props.date) {
+      return (
+        <Moment element="h5" format=" dddd[na]" locale="fi">
+          {props.date}
+        </Moment>
+      )
+    }
+  }
+
   const duration = () => {
     return (
       <Moment element="span" diff={props.date} unit="hours">
-          {props.endingDate}
+        {props.endingDate}
       </Moment>
     )
+  }
+
+  const description = () => {
+    if (props.desc) {
+      return <p>{ReactHtml(props.desc.replace(/\n/g, '<br />'))}</p>
+    } else {
+      return <em>{sutkautuksia[Math.floor(Math.random() * sutkautuksia.length)]}</em>
+    }
   }
 
   return (
@@ -44,22 +69,24 @@ export default function Event(props) {
           </div>
 
           <div className="event_location">
-            <span className="location">{location()}</span>
+            <span className="day">{day()}</span>
             <span className="date">{date()}</span>
-            <span className="duration"><h5>{duration()} h</h5></span>
+            <span className="location">{location()}</span>
+            {/*
+            <span className="duration">
+              <h5>~ {duration()} h</h5>
+            </span>
+            */}
           </div>
 
-          {props.desc && (
-            <div className="event_desc">
-              {/*korvataan rivinvaihdot <br>*/}
-              <p>{ReactHtml(props.desc.replace(/\n/g, '<br />'))}</p>
-            </div>
-          )}
+          <div className="event_desc">
+            {description()}
+          </div>
 
           <div className="event_footer">
             <div className="event_more">
               <a href={props.url} className="btn_more">
-                Lue lisää
+                Kalenteriin
               </a>
             </div>
           </div>
